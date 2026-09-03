09c6cb954376efc197cdee60041b3319a8fe633c
Nomita Sen <nomitasen85@gmail.com>
2026-09-03 21:39:23 +0530

    feat: learn bitwise shift operators

---
+++ 01_Basics/shift_operators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 8;
+    //left shift (multiply by 2 ) 
+    cout<<"n << 1: "<<(n << 1)<<endl;
+    cout<<"n <<2: "<<(n << 2)<<endl;
+    //right shift(divide by 2)
+    cout<<"n >> 1: "<<(n >> 1)<<endl;
+    cout<<"n >> 2: "<<(n >> 2)<<endl;
+    return 0;
+
+}
