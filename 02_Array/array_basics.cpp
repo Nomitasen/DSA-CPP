89cc3686fa3d449335f1db0db382114e5272e720
Nomita Sen <nomitasen85@gmail.com>
2026-09-05 22:26:55 +0530

    feat: add array basics example

---
+++ 02_Array/array_basics.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[5] ={22, 44, 55, 77 ,64};
+    cout <<"Enter elements of Array are: ";
+
+    for(int i = 0; i < 5; i++){
+        cout <<marks[i] << " ";
+    }
+    cout << endl;
+    return 0;
+
+
+    
+    
+}
